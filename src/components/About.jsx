62e6cb71705a21a3a447ed32a59058e7b2897db5
Undefined <karimullaskmk1234@gmail.com>
2026-01-21 14:08:35 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,9 +49,9 @@ const About = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-4 text-secondary text-[18px] max-w-3xl leading-[30px] font-semibold'
         >
-          I'm a skilled software developer with experience in TypeScript and
-          JavaScript, and expertise in frameworks like React, Node.js, and
-          Three.js. I'm a quick learner and collaborate closely with clients to
+          I'm a skilled web developer with experience in TypeScript and
+          JavaScript, and expertise in frameworks like Metasploit and Node.js.
+          I'm a quick learner and collaborate closely with clients to
           create efficient, scalable, and user-friendly solutions that solve
           real-world problems. Let's work together to bring your ideas to life!
         </motion.p>
