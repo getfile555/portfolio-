e1654dad2b685089b456a693bfdc0bc54406be11
Undefined <karimullaskmk1234@gmail.com>
2026-01-21 11:51:59 +0530

    Create constants.js

---
+++ src/constants/constants.js
@@ -0,0 +1,399 @@
+import {
+  mobile,
+  backend,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  carrent,
+  jobit,
+  inkSync,
+  tracker,
+  threejs,
+  nextjs,
+  express,
+  java,
+  jwt,
+  nextauth,
+  socialSphere,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Programmer",
+    icon: backend,
+  },
+  // {
+  //   title: "Creator",
+  //   icon: creator,
+  // },
+];
+
+const technologies = [
+  {
+    name: "React JS",
+    icon: reactjs,
+    position: [0, 0, 0],
+  },
+  {
+    name: "Next JS",
+    icon: nextjs,
+    position: [2, 0, 0],
+  },
+  {
+    name: "Express JS",
+    icon: express,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "JWT",
+    icon: jwt,
+  },
+  // {
+  //   name: "NextAuth",
+  //   icon: nextauth,
+  // },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const projects = [
+  {
+    name: "Tool-kit",
+    description:
+      "A real-time education information gathering web application built with Javascript, Python, and Html.",
+    features: [
+      "Users can get some basic information of any persom..",
+      "It is an high complegated and cyber tool to find ip location etc....",
+    ],
+    tags: [
+      {
+        name: "Javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Python",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Html",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Toolkit,
+    source_code_link: "https://github.com/getfile555/Tool-kit.git",
+    live_link: "https://tool-kit--itscode2762.replit.app",
+  },
+  {
+    name: "Vechile-info",
+    description:
+      "A dynamic full-stack education RC details gathering tool...",
+    features: [
+      "It's an full-stack web app to get details.",
+      "Simple and easy to use its an open source.",
+      "User gets information form an API.",
+      "Displays the RC details to user.",
+    ],
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Fast-API",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Java",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Python",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "Database",
+        color: "blue-text-gradient",
+      },
+      
+    ],
+    image: Vechile,
+    source_code_link: "https://github.com/getfile555/Vechile-info.git",
+    live_link: "https://github.com/getfile555/Vechile-info.git",
+  },
+  {
+    name: "Portfolio",
+    description: "Deveploed a webstie for introudcing my-self.",
+    features: [
+      "It shows my detils and provide user to contact",
+      "Students can get source code and use it for there portfolios",
+      "A website with simple and beautiful animaions",
+      "PWA (Progressive Web App) support.",
+    ],
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ExpressJs",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "javascript",
+        color: "green-text-gradient",
+      },
+    ],
+    image: portfolio,
+    source_code_link: "https://github.com/getfile555/portfolio-.git",
+    live_link: "https://studynotion-master.vercel.app/",
+  },
+  {
+    name: "Birthday-site",
+    description:
+      "It's simple and beautiful webstie for wishes..",
+    features: [
+      "Users can add coustimise images to the site..",
+      "We can made some one special.",
+    ],
+    tags: [
+      {
+        name: "Java",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Chrome Extension",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Html",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Birthday,
+    source_code_link: "https://github.com/himanshu8443/FlipKart-Price-Tracker",
+    live_link:
+      "birthday1-1.netlify.app",
+  },
+  {
+    name: "Social Sphere",
+    description:
+      "A full-stack social media application with user authentication, infinite scroll, and image and video uploads.",
+    features: [
+      "User authentication using JWT and OAuth providers.",
+      "Infinite scroll for posts.",
+      "Image and video uploads.",
+      "Like, comment, and share posts.",
+      "Follow and unfollow users.",
+      "Dark mode support.",
+    ],
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Supabase",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: socialSphere,
+    source_code_link: "https://github.com/himanshu8443/SocialSphere",
+    live_link: "https://socialspherex.vercel.app/",
+  },
+];
+
+// const experiences = [
+//   {
+//     title: "React.js Developer",
+//     company_name: "Starbucks",
+//     icon: starbucks,
+//     iconBg: "#383E56",
+//     date: "March 2020 - April 2021",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "React Native Developer",
+//     company_name: "Tesla",
+//     icon: tesla,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2021 - Feb 2022",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Web Developer",
+//     company_name: "Shopify",
+//     icon: shopify,
+//     iconBg: "#383E56",
+//     date: "Jan 2022 - Jan 2023",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Full stack Developer",
+//     company_name: "Meta",
+//     icon: meta,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2023 - Present",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+// ];
+
+// const testimonials = [
+//   {
+//     testimonial:
+//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+//     name: "Sara Lee",
+//     designation: "CFO",
+//     company: "Acme Co",
+//     image: "https://randomuser.me/api/portraits/women/4.jpg",
+//   },
+//   {
+//     testimonial:
+//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
+//     name: "Chris Brown",
+//     designation: "COO",
+//     company: "DEF Corp",
+//     image: "https://randomuser.me/api/portraits/men/5.jpg",
+//   },
+//   {
+//     testimonial:
+//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//     name: "Lisa Wang",
+//     designation: "CTO",
+//     company: "456 Enterprises",
+//     image: "https://randomuser.me/api/portraits/women/6.jpg",
+//   },
+// ];
+
+export {
+  services,
+  technologies,
+  //  experiences,
+  //  testimonials,
+  projects,
+};
